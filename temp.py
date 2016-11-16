1b0f652992304071170550b76c421dac2c3e99c7
Rob Willisonw <Rob@willison.co.uk>
2016-11-16 16:53:30 +0000

    more stuff

--- temp.py
+++ temp.py
@@ -3,26 +3,20 @@
 first = True
 core1 = 0
 results = []
-string = "50x50"
-f = open('KarpFlatt/' + string,'w')
-f.write('cores time karpflatt\n')
-with open(string, "r") as my_file:
+string = "100"
+f = open('Isoefficiency/New/' + string,'w')
+f.write('cores time efficiency\n')
+with open("Isoefficiency/" + string, "r") as my_file:
     for lines in my_file:
-        if first:
-            first = False
-            continue
-
         lines = lines.replace("\n", '');
         numbers = lines.split(' ')
 
         if int(numbers[0]) == 1:
             core1 = float(numbers[1])
-            continue
 
-        top = ((1 / float(numbers[2])) - (1 / float(numbers[0])))
-        bottom = 1 - (1 / float(numbers[0]))
-        numbers.append(top / bottom)
+        p = float(numbers[0])
+        numbers.append((core1 / (p * float(numbers[1]))))
 
-        f.write(str(numbers[0]) + ' ' + str(numbers[1]) + ' ' + str(numbers[3]) + '\n')
+        f.write(str(numbers[0]) + ' ' + str(numbers[2]) + '\n')
 
 f.close()
